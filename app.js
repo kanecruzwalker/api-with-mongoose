4690a1212c8bf74dc3bf52d64637f0e80045a441
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-19 03:23:01 -0700

    loging response from get route, can access model and use methods with it

--- app.js
+++ app.js
@@ -7,27 +7,38 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
-
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const articleSchema = {
-    title: {
-      type: String, 
-      requried: true
-    },
-    content: {
-      type: String,
-      required: true
-    }
-  }
+  title: {
+    type: String,
+    requried: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+};
 
-  const Article = mongoose.model("Article", articleSchema);
+const Article = mongoose.model("Article", articleSchema);
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
-})
+app.get("/articles", function (req, res) {
+  Article.find(function (err, foundArticles) {
+    console.log(foundArticles);
+  });
+  console.log(req);
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
